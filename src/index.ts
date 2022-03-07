a2d80ab6cf4bddccd004b9b4ca624c526176ee13
JacobLinCool <jacoblincool@gmail.com>
2022-03-08 03:26:04 +0800

    build: no minification

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import path from "node:path";
 import chalk from "chalk";
 import { program } from "commander";
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from "tsup";
 export default defineConfig((options) => ({
     entry: ["src/index.ts"],
     outDir: "lib",
-    target: "node16",
+    target: "node14",
     format: ["cjs", "esm"],
     clean: true,
     splitting: false,
-    minify: !options.watch,
-    dts: options.watch ? false : { resolve: true },
+    // minify: !options.watch,
+    // dts: options.watch ? false : { resolve: true },
 }));
